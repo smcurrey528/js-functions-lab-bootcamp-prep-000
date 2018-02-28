3bbd5b2178c170e77fc71549d5a9324db565cf67
Sarah Currey <curreysarah28@gmail.com>
2018-02-28 03:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays () {
 }
 function happyHolidaysTo (name) {
   console.log ('Happy holidays, ${name}!');
-  return `Happy holidays,Sarah!`;
+  return `Happy holidays, you!`;
 }
 function say(holidayCountdown) {
   console.log ("It's ${days} days until ${holidays}!")
