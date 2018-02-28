73038e594690458b7157902e1607a2bc2859bb6f
Sarah Currey <curreysarah28@gmail.com>
2018-02-28 03:32:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-function happyHolidays {
+function happyHolidays () {
   console.log('Happy Holidays')
-  return ('Happy Holidays!')
+  return (Happy Holidays!')
 }
 function say(happyHolidaysTo, firstName) {
   console.log ('${Happy holidays}, ${first name}!')
